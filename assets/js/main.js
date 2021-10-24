bc78d4e6865666a0b3813dce814a51016a3357bb
Gladson <gladsonfinamore@gmail.com>
2021-10-24 13:39:19 -0300

    App concluido

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+function Calculadora() {
+  this.display = document.querySelector('.display')
+
+  this.inicia = () => {
+    this.capturaCliques();
+    this.capturaEnter();
+  }
+
+  this.capturaEnter = () => {
+    this.display.addEventListener('keypress', e => {
+      if(e.keyCode !== 13) return;
+      this.realizaConta()
+    })
+  }
+
+  this.capturaCliques = () => {
+    document.addEventListener('click', e => {
+      const el = e.target
+      if(el.classList.contains('btn-num')) this.addNum(el)
+      if(el.classList.contains('btn-clear')) this.clear()
+      if(el.classList.contains('btn-del')) this.del(el)
+      if(el.classList.contains('btn-eq')) this.realizaConta(el)
+    })
+  }
+
+  this.realizaConta = () => {
+    try {
+      const conta = eval(this.display.value);
+
+      if(!conta){
+        alert('Conta Invalida')
+        return
+      }
+      this.display.value = conta
+    } catch (error) {
+      alert('Conta invalida')
+      return;      
+    }
+  }
+
+  this.addNum = el => {
+    this.display.value += el.innerText
+    this.display.focus();
+  };
+  this.clear = () => this.display.value = '';
+  this.del = () => this.display.value = this.display.value.slice(0, -1) 
+  
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia()
